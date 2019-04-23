dd716411bd48ca9d910807674afada7ef8471019
mayudi <1012963299@qq.com>
2019-04-23 23:47:24 +0800

    轮播图实现

--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 new Vue({
   el: '#app',
   router,
