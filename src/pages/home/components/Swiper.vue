3382cf74fe13c1d5529030e55c8e6fe2d088bd3d
mayudi <1012963299@qq.com>
2019-04-27 23:34:50 +0800

    图标功能

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -16,7 +16,8 @@ export default {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true
+        loop: true,
+        autoplay: 2500
       },
       swiperList: [{
         id: '0001',
