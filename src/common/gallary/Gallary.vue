c0afe80a97987efb213ccca73a7720da11191afa
mayudi <mayudi@flashhold.com>
2019-05-25 18:45:56 +0800

    详情功能

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -1,10 +1,42 @@
 <template>
-    
+    <div class="container" @click="handleGalleryClick">
+      <div class="wrapper" >
+        <swiper :options="swiperOptions">
+          <swiper-slide v-for="(item ,index) in imgs" :key="index">
+            <img :src="item" class="gallary-img">
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+      </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Gallary"
+      name: "CommonGallary",
+      props: {
+        imgs: {
+          type:Array,
+          default() {
+            return []
+          }
+        }
+      },
+      data () {
+        return {
+          swiperOptions: {
+            pagination: '.swiper-pagination',
+            paginationType: 'fraction',
+            observeParents: true,
+            observer: true
+          }
+        }
+      },
+      methods: {
+        handleGalleryClick(){
+          this.$emit('close')
+        }
+      }
     }
 </script>
 
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
-    <div class="banner">
+    <div class="banner" @click="handleBannerClick">
       <img class="banner-img" src="bannerImg">
       <div class="banner-info">
         <div class="banner-title">{{this.sightName}}</div>
         <div class="banner-number">{{this.bannerImgs.length}}</div>
       </div>
     </div>
+    <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGalleryClose"></common-gallary>
   </div>
 
 </template>
 
 <script>
+  import CommonGallary from 'common/gallery/Gallery'
+
   export default {
     name: "DetailBanner",
+    comments: {
+      CommonGallary
+    },
     props: {
       sightName: String,
       bannerImg: String,
       bannerImgs: Array
+    },
+    data() {
+      return {
+        showGallary: false
+      }
+    },
+    methods: {
+      handleGalleryClose() {
+        this.showGallary = false
+      },
+      handleBannerClick() {
+        this.showGallary = true
+      }
     }
   }
 </script>
